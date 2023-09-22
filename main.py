554d5b8d732624360ba0bcc8258a0530c90fdcac
milosnowcat <ramiro.alvarez@rahcode.com>
2023-09-21 18:06:34 -0600

    mail sender

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 # TODO hacer un programa de recordatorios con correo electronico,
 # pedira clave de correo al usuario y podra hacer entradas con fecha,
 # que sera cuando se le enviara el correo.
+
+import utils
+from getpass import getpass
+
+def main():
+    sender = input("Your gmail: ")
+    password = getpass()
+    recipient = input("Send mail to: ")
+    subject = input("Subject: ")
+    body = input("Body: ")
+
+    utils.send_mail(sender, password, recipient, subject, body)
+    print("Message sent!")
+
+main()
